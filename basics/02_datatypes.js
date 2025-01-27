7ecdcc0dfc72fa6bf60d9a9499fbf5af5490e144
Agniva2006 <agnivaghosh2006@gmail.com>
2025-01-27 10:00:51 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// alert("hello")
+console.log(3+3)
+
+console.log("Hitesh")
+let name = "hitesh"
+let age =18
+let isLoggedIn=false 
+
+//number => false
+//bigint
+//string =>" "
+//boolean=>true/false
+//null =>standalone value//object
+//undefined =>
+//symbol=>unique
+
+//object
+
+console.log(typeof"hitesh");
+console.log(typeof null);
+console.log(typeof false);
